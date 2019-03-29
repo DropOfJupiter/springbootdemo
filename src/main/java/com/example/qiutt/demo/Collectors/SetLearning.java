4e71163cd6b489ad3a2b1a1b3fb36ece123d66c9
邱恬恬 <qiutt@kaopuyun.com>
2019-03-29 12:03:53 +0800

    Set存放不重复的对象，需要重写对象的equals和hasCode方法

--- src/main/java/com/example/qiutt/demo/Collectors/SetLearning.java
+++ src/main/java/com/example/qiutt/demo/Collectors/SetLearning.java
@@ -1,12 +1,17 @@
 package com.example.qiutt.demo.Collectors;
 
+import cn.hutool.core.collection.CollectionUtil;
+import cn.hutool.core.util.StrUtil;
 import cn.hutool.json.JSONUtil;
 import com.example.qiutt.demo.stream.ConsumptPayType;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.Test;
 
-import java.util.HashSet;
-import java.util.Set;
+import java.util.*;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.function.Function;
+import java.util.function.Predicate;
+import java.util.stream.Collectors;
 
 /**
  * @author qiutt
@@ -26,6 +31,36 @@ public class SetLearning {
 		resourceUUIDAndType1=new ResourceUUIDAndType("111",ConsumptPayType.ALIPAY_CONSUMPT);
 		set.add(resourceUUIDAndType1);
 		log.info(JSONUtil.toJsonStr(set));
+
+		List<ResourceUUIDAndType> list=new ArrayList<>();
+		resourceUUIDAndType1=new ResourceUUIDAndType("111", ConsumptPayType.ALIPAY_CONSUMPT);
+		list.add(resourceUUIDAndType1);
+		resourceUUIDAndType1=new ResourceUUIDAndType("222",ConsumptPayType.BALANCE_CONSUMPT);
+		list.add(resourceUUIDAndType1);
+		resourceUUIDAndType1=new ResourceUUIDAndType("111",ConsumptPayType.ALIPAY_CONSUMPT);
+		list.add(resourceUUIDAndType1);
+		list=list.stream().distinct().collect(Collectors.toList());
+		log.info(JSONUtil.toJsonStr(list));
+
+		list=list.stream().filter(distinctByKey(item->item.getResourceUUID())).collect(Collectors.toList());
+		log.info(JSONUtil.toJsonStr(list));
+		HashSet<ConsumptPayType> ConsumptPayTypeList=new HashSet<ConsumptPayType>();
+		ConsumptPayType consumptPayType=null;
+		for (ResourceUUIDAndType resourceUUIDAndType : list) {
+			if(consumptPayType!=null){
+				if(consumptPayType.equals(resourceUUIDAndType.getType())){
+					continue;
+				}else{
+					consumptPayType = resourceUUIDAndType.getType();
+				}
+			}else{
+				consumptPayType = resourceUUIDAndType.getType();
+			}
+			//ResourceUUIDAndType没有重写equals和hashCode，所以用这个方式来去重
+			ConsumptPayTypeList.add(consumptPayType);
+		}
+		log.info(JSONUtil.toJsonStr(ConsumptPayTypeList));
+
 	}
 
 	class ResourceUUIDAndType{
@@ -53,22 +88,27 @@ public class SetLearning {
 			this.type = type;
 		}
 
-		@Override
-		public boolean equals(Object obj) {
-			if(!(obj instanceof ResourceUUIDAndType)) {
-				return false;
-			}
-			ResourceUUIDAndType b = (ResourceUUIDAndType)obj;
-			if(this.resourceUUID.equals(b.getResourceUUID())&&this.type.equals(b.getType())) {
-				return true;
-			}
-			return false;
-		}
+//		@Override
+//		public boolean equals(Object obj) {
+//			if(!(obj instanceof ResourceUUIDAndType)) {
+//				return false;
+//			}
+//			ResourceUUIDAndType b = (ResourceUUIDAndType)obj;
+//			if(this.resourceUUID.equals(b.getResourceUUID())&&this.type.equals(b.getType())) {
+//				return true;
+//			}
+//			return false;
+//		}
+//
+//		@Override
+//		public int hashCode() {
+//			log.info("type.hashCode()："+type.hashCode());
+//			return type.hashCode();
+//		}
+	}
 
-		@Override
-		public int hashCode() {
-			log.info("type.hashCode()："+type.hashCode());
-			return type.hashCode();
-		}
+	private static <T> Predicate<T> distinctByKey(Function<? super T, ?> keyExtractor) {
+		Map<Object,Boolean> seen = new ConcurrentHashMap<>();
+		return t -> seen.putIfAbsent(keyExtractor.apply(t), Boolean.TRUE) == null;
 	}
 }
